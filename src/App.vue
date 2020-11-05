08f3ee3b0ec9cb62981dbb90d67d82cd18936ced
mauger <mickael.auger@apside-groupe.com>
2020-11-05 08:31:59 +0100

    [Cours-14] Création de la page d'accueil

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,81 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <b-navbar toggleable type="dark" variant="dark">
+      <b-navbar-brand href="#">Mickaël</b-navbar-brand>
+    </b-navbar>
+
+    <div class="container contenu">
+      <h1>Bienvenue sur mon site !</h1>
+      <div class="row">
+        <div class="col-sm-4">
+          <div>
+            <b-card
+                title="Montain bike"
+                img-src="./images/mountain_bike.jpg"
+                img-alt="Image"
+                img-top
+                tag="article"
+                style="max-width: 20rem;"
+                class="mb-2"
+            >
+              <b-card-text>
+                Mes plus belles ballades !
+              </b-card-text>
+
+              <b-button href="#" variant="primary">Voir</b-button>
+            </b-card>
+          </div>
+        </div>
+
+        <div class="col-sm-4">
+          <div>
+            <b-card
+                title="Tango"
+                img-src="./images/tango.jpg"
+                img-alt="Image"
+                img-top
+                tag="article"
+                style="max-width: 20rem;"
+                class="mb-2"
+            >
+              <b-card-text>
+                Mes plus belles photos de tango.
+              </b-card-text>
+
+              <b-button href="#" variant="primary">Voir</b-button>
+            </b-card>
+          </div>
+        </div>
+
+        <div class="col-sm-4">
+          <div>
+            <b-card
+                title="Montagne"
+                img-src="./images/mountain.jpg"
+                img-alt="Image"
+                img-top
+                tag="article"
+                style="max-width: 20rem;"
+                class="mb-2"
+            >
+              <b-card-text>
+                Mes photos de montagnes.
+              </b-card-text>
+
+              <b-button href="#" variant="primary">Voir</b-button>
+            </b-card>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: 'App'
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
