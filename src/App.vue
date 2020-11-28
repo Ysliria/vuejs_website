3765d598b1481ec8cae84b37cd37537721636f2b
mauger <auger.mickael37@gmail.com>
2020-11-28 09:13:24 +0100

    Ajout du premier composant et du router

--- src/App.vue
+++ src/App.vue
@@ -1,72 +1,15 @@
 <template>
   <div id="app">
-    <b-navbar toggleable type="dark" variant="dark">
+    <b-navbar type="dark" variant="dark">
       <b-navbar-brand href="#">Mickaël</b-navbar-brand>
+      <b-navbar-nav>
+        <b-nav-item href="/">Accueil</b-nav-item>
+        <b-nav-item href="/about">A propos</b-nav-item>
+      </b-navbar-nav>
     </b-navbar>
 
     <div class="container contenu">
-      <h1>Bienvenue sur mon site !</h1>
-      <div class="row">
-        <div class="col-sm-4">
-          <div>
-            <b-card
-                title="Montain bike"
-                img-src="./images/mountain_bike.jpg"
-                img-alt="Image"
-                img-top
-                tag="article"
-                style="max-width: 20rem;"
-                class="mb-2"
-            >
-              <b-card-text>
-                Mes plus belles ballades !
-              </b-card-text>
-
-              <b-button href="#" variant="primary">Voir</b-button>
-            </b-card>
-          </div>
-        </div>
-
-        <div class="col-sm-4">
-          <div>
-            <b-card
-                title="Tango"
-                img-src="./images/tango.jpg"
-                img-alt="Image"
-                img-top
-                tag="article"
-                style="max-width: 20rem;"
-                class="mb-2"
-            >
-              <b-card-text>
-                Mes plus belles photos de tango.
-              </b-card-text>
-
-              <b-button href="#" variant="primary">Voir</b-button>
-            </b-card>
-          </div>
-        </div>
-
-        <div class="col-sm-4">
-          <div>
-            <b-card
-                title="Montagne"
-                img-src="./images/mountain.jpg"
-                img-alt="Image"
-                img-top
-                tag="article"
-                style="max-width: 20rem;"
-                class="mb-2"
-            >
-              <b-card-text>
-                Mes photos de montagnes.
-              </b-card-text>
-
-              <b-button href="#" variant="primary">Voir</b-button>
-            </b-card>
-          </div>
-        </div>
-      </div>
+      <router-view></router-view>
     </div>
   </div>
 </template>
---
+++ src/components/pages/About.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>A propos</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur dolore et porro provident! Aspernatur doloribus exercitationem numquam quod totam! Accusantium at eum itaque iure magni pariatur perspiciatis, ratione reprehenderit vitae.</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aspernatur ea natus nobis temporibus. Amet at commodi corporis dolor, dolore error et fugit harum laudantium libero minima nostrum, sint vitae?</p>
+    <p>
+      <router-link to="/">
+        Retour à la page d'accueil.
+      </router-link>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+
+</style>
